d3aaa0f5c2cac2de47544ab998e3a3ddb8d96fcf
Alex Zolin <sashadart@yandex.ru>
2019-02-25 00:47:13 +0300

    commit1

--- src/test/java/org/andestech/learning/rfb18/g2/AppTest.java
+++ src/test/java/org/andestech/learning/rfb18/g2/AppTest.java
@@ -2,17 +2,59 @@ package org.andestech.learning.rfb18.g2;
 
 
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 
 public class AppTest
 {
-    @Test
-    public void shouldAnswerWithTrue()
-    {
 
-        assertTrue( true );
+    public static int testInt = 10;
 
+    @Test(dependsOnMethods = "meth05Test")
+    public void meth01Test(){
+
+        System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
+        assertEquals(testInt, 10);
 
     }
+
+
+    @Test()
+    public void meth02Test(){
+
+        System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
+        assertEquals(testInt, 10);
+
+    }
+
+
+    @Test()
+    public void meth03Test(){
+
+        System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
+        assertEquals(testInt, 10);
+
+    }
+
+
+    @Test()
+    public void meth04Test(){
+
+        System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
+        assertEquals(testInt, 10);
+
+    }
+
+
+    @Test(dependsOnMethods = "meth04Test")
+    public void meth05Test(){
+
+        System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
+        assertEquals(testInt, 10);
+
+    }
+
+
 }
