7334bc1e357a8a6567a5a3f940dc522cf22a847d
AntonB <mortician233@gmail.com>
2019-02-25 20:05:57 +0300

    home work

--- src/test/java/org/andestech/learning/rfb18/g2/AppTest.java
+++ src/test/java/org/andestech/learning/rfb18/g2/AppTest.java
@@ -1,18 +1,45 @@
 package org.andestech.learning.rfb18.g2;
 
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
-import static org.testng.Assert.assertTrue;
 
 
 public class AppTest
 {
-    @Test
-    public void shouldAnswerWithTrue()
+    @Test(dependsOnMethods = "meth05Test")
+    public void meth01Test()
     {
+        Assert.assertEquals(1, 3);
+        Thread.currentThread().getStackTrace()[1].getClassName();
 
-        assertTrue( true );
+    }
 
+    @Test
+    public void meth02Test() {
+        Assert.assertEquals(1, 1);
+        Thread.currentThread().getStackTrace()[1].getClassName();
+
+    }
+
+    @Test
+    public void meth03Test() {
+        Assert.assertEquals(1, 1);
+        Thread.currentThread().getStackTrace()[1].getClassName();
+
+    }
+
+    @Test
+    public void meth04Test() {
+        Assert.assertEquals(1, 1);
+        Thread.currentThread().getStackTrace()[1].getClassName();
+
+    }
+
+    @Test(dependsOnMethods = "meth04Test")
+    public void meth05Test() {
+        Assert.assertEquals(1, 1);
+        Thread.currentThread().getStackTrace()[1].getClassName();
 
     }
 }
