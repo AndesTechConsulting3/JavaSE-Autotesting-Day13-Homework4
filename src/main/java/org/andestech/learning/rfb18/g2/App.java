b56725dd83e2381acbb4714aa1e3bb307831a8c0
MihaRF <miharf@gmail.com>
2019-02-26 20:54:16 +0300

    comparing arrays (for me)

--- src/main/java/org/andestech/learning/rfb18/g2/App.java
+++ src/main/java/org/andestech/learning/rfb18/g2/App.java
@@ -1,13 +1,31 @@
 package org.andestech.learning.rfb18.g2;
 
+import java.util.Arrays;
+
 /**
- * Hello world!
- *
+ * Сomparing arrays
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int N = 10;
+        int start = 0;
+        String[] s1 = new String[N];
+        for (int i = start; i < N; i++)
+            s1[i] = "q" + i;
+        System.out.println(Arrays.asList(s1));
+        String[] s2 = new String[N];
+        for (int i = start; i < N; i++)
+            s2[i] = "q" + i*i;
+        System.out.println(Arrays.asList(s2));
+
+        for (int i=start; i < N; i++){
+            for (int j=start; j<N; j++)
+            {
+                if (s1[i].equals(s2[j]))
+                    System.out.println("Matched! - " + s1[i]);
+            }
+        }
     }
 }
