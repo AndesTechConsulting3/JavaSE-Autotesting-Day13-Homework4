e25f96971d1eda2a8ed13bf64c09a44a704d20c9
ellena2905 <yelena_2905@mail.ru>
2019-02-28 00:56:37 +0300

    homework

--- src/test/java/org/andestech/learning/rfb18/g2/AppTest.java
+++ src/test/java/org/andestech/learning/rfb18/g2/AppTest.java
@@ -1,18 +1,64 @@
 package org.andestech.learning.rfb18.g2;
 
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.annotations.*;
+
+import static java.lang.Math.pow;
 import static org.testng.Assert.assertTrue;
 
-
+/**
+ * Unit test for simple App.
+ */
 public class AppTest
 {
+    int a = 1;
+    int b = 2;
+    int c=3;
+
+    @Test(dependsOnMethods = "meth05Test")
+    public void meth01Test() {
+
+        int expectedValue = 1;
+        int actualValue = a * b * c;
+        Assert.assertEquals(actualValue, expectedValue);
+        System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
+    public void meth02Test(){
 
-        assertTrue( true );
+        double expectedValue = 9;
+        double actualValue = (pow(b, c)) + a;
+        Assert.assertEquals(actualValue, expectedValue);
+        System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
+    }
 
+    @Test
+    public void meth03Test(){
 
+        int expectedValue = 6;
+        int actualValue = a + b + c;
+        Assert.assertEquals(actualValue, expectedValue);
+        System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
+    }
+
+    @Test(priority = -1)
+    public void meth04Test(){
+
+        double expectedValue = 16;
+        double actualValue = (pow(b, a+c));
+        Assert.assertEquals(actualValue, expectedValue);
+        System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
+    }
+
+    @Test(dependsOnMethods = "meth04Test")
+    public void meth05Test(){
+
+        int expectedValue = 7;
+        int actualValue = a + b * c;
+        Assert.assertEquals(actualValue, expectedValue);
+        System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
     }
 }
