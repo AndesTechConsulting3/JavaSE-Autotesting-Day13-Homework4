b31b989876fa05820060d493d7f76ac0ef103c13
VitalikMetallik <flash_PS4@mail.ru>
2019-03-06 14:12:53 +0300

    Homework5

--- src/test/java/org/andestech/learning/rfb18/g2/AppTest.java
+++ src/test/java/org/andestech/learning/rfb18/g2/AppTest.java
@@ -2,17 +2,47 @@ package org.andestech.learning.rfb18.g2;
 
 
 import org.testng.annotations.Test;
-import static org.testng.Assert.assertTrue;
+
+import static org.testng.Assert.assertEquals;
 
 
 public class AppTest
 {
+
+    private int homework5 = 1;
+
+    @Test(dependsOnMethods = "meth05Test")
+
+    public void meth01Test() {
+        System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
+        assertEquals(homework5, 2);
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
 
-        assertTrue( true );
+    public void meth02Test() {
+        System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
+        assertEquals(homework5, 2);
+    }
 
+    @Test
 
+    public void meth03Test() {
+        System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
+        assertEquals(homework5, 2);
+    }
+
+    @Test
+
+    public void meth04Test() {
+        System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
+        assertEquals(homework5, 2);
+    }
+
+    @Test
+            (dependsOnMethods = "meth04Test")
+    public void meth05Test() {
+        System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
+        assertEquals(homework5, 2);
     }
 }
